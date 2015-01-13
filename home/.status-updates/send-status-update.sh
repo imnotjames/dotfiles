caa065c0529b5853c34ed92e9bb0f81be5fe2fba
James Ward <james@notjam.es>
2015-01-13 15:17:05 -0500

    fix a few issues with script
    wrap variables in quotes and fix wrong variable

--- home/.status-updates/send-status-update.sh
+++ home/.status-updates/send-status-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TO_EMAILS=$(< recipients.conf)
+TO_EMAILS="$(< recipients.conf)"
 
 TODAYS_MESSAGE_FILE="message/status/$(date +%d-%m-%Y).txt"
 
@@ -8,17 +8,19 @@ TEMPLATE="$(< message/template.txt)"
 DEFAULT_MESSAGE="$(< message/default.txt)"
 
 if [[ -f "$TODAYS_MESSAGE_FILE" ]]; then
-	MESSAGE="$(cat $TODAYS_STATUS_MESSAGE_FILE | sed -e 's/^ *//' -e 's/ *$//')"
+	MESSAGE="$(cat $TODAYS_MESSAGE_FILE | sed -e 's/^ *//' -e 's/ *$//')"
 else
-	MESSAGE+""
+	MESSAGE=""
 fi
 
 
 if [[ -z "$MESSAGE" ]]; then
-	MESSAGE=$DEFAULT_MESSAGE
+	MESSAGE="$DEFAULT_MESSAGE"
 fi
 
 COMPLETE_MESSAGE=$(printf "$TEMPLATE" "$MESSAGE")
+
+echo "Sending status update to ${TO_EMAILS}"
 
 echo "$COMPLETE_MESSAGE" | mail \
 	-s "[Deploys] Status and Deploy Update for $(date +%D)" \
