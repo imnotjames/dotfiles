85f046b640d37d659233ad12704f5dbf08f944f6
James Ward <james@notjam.es>
2015-01-14 10:35:50 -0500

    make status-updates more configurable
    move subject into config, allow setting BCC as well as TO addresses
    also make sure the files are loaded a bit more absolutely

--- home/.status-updates/send-status-update.sh
+++ home/.status-updates/send-status-update.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
+CONFIGURATION_FILE="$HOME/.status-updates/status-updates.conf"
 
-cd $HOME/.status-updates
+TEMPLATE_FILE="$HOME/.status-updates/message/template.txt"
+DEFAULT_MESSAGE_FILE="$HOME/.status-updates/message/default.txt"
+TODAYS_MESSAGE_FILE="$HOME/.status-updates/message/status/$(date +%d-%m-%Y).txt"
 
-TO_EMAILS="$(< recipients.conf)"
+if [ ! -f "$CONFIGURATION_FILE" ]; then
+	echo "Error: configuration file missing"
+	exit
+fi
 
-TODAYS_MESSAGE_FILE="message/status/$(date +%d-%m-%Y).txt"
+. "$CONFIGURATION_FILE"
 
-TEMPLATE="$(< message/template.txt)"
-DEFAULT_MESSAGE="$(< message/default.txt)"
+TEMPLATE="$(< $TEMPLATE_FILE)"
+DEFAULT_MESSAGE="$(< $DEFAULT_MESSAGE_FILE)"
 
 if [[ -f "$TODAYS_MESSAGE_FILE" ]]; then
 	MESSAGE="$(cat $TODAYS_MESSAGE_FILE | sed -e 's/^ *//' -e 's/ *$//')"
@@ -15,15 +21,29 @@ else
 	MESSAGE=""
 fi
 
-
 if [[ -z "$MESSAGE" ]]; then
 	MESSAGE="$DEFAULT_MESSAGE"
 fi
 
+if [[ -z "$SUBJECT" ]]; then
+	SUBJECT="Status Update for %D"
+fi
+
+SUBJECT=$(date +"$SUBJECT")
+
 COMPLETE_MESSAGE=$(printf "$TEMPLATE" "$MESSAGE")
+
+if [[ -z "$TO_EMAILS" ]]; then
+	echo "Error: No emails to send to"
+
+	exit 1
+fi
 
 echo "Sending status update to ${TO_EMAILS}"
 
 echo "$COMPLETE_MESSAGE" | mail \
-	-s "[Deploys] Status and Deploy Update for $(date +%D)" \
-	${TO_EMAILS}
+	-s "$SUBJECT" \
+	-b "${BCC_EMAILS}" \
+	"${TO_EMAILS}"
+
+exit $?
