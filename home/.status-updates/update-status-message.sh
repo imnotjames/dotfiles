72685cf7917a1f1d4eb9b2f42af4f4803c65a3f3
James Ward <james@notjam.es>
2015-02-01 11:15:26 -0500

    update usability of update-status-message
    Adds -d option to update a date other than today
    Adds -m option for passing in message
    Adds an editor prefix

--- home/.status-updates/update-status-message.sh
+++ home/.status-updates/update-status-message.sh
@@ -1,23 +1,57 @@
 #!/usr/bin/env bash
 
-TODAYS_MESSAGE_FILE="$HOME/.status-updates/message/status/$(date +%d-%m-%Y).txt"
+CHOSEN_DATE="$(date +%d-%m-%Y)"
+MESSAGE=""
+
+OPTIONS=`getopt -o d::m:: --long date:message: -n "update-status-message" -- "$@"`
+eval set -- "$OPTIONS"
+
+while true; do
+	case "$1" in
+		-d|--date)
+			CHOSEN_DATE="$(date --date="$2" +%d-%m-%Y)"
+
+			if [[ "$?" != "0" ]]; then
+				exit 1
+			fi
+
+			shift 2
+			;;
+		-m|--message)
+			MESSAGE="$2"
+			shift 2
+			;;
+		"--")
+			shift
+			break
+			;;
+		*)
+			echo "Internal error: $1"
+			exit 1
+			;;
+	esac
+done
+
+CHOSEN_MESSAGE_FILE="$HOME/.status-updates/message/status/$CHOSEN_DATE.txt"
+EDITOR_MESSAGE_FILE="$HOME/.status-updates/message/status-editor-message.txt"
 
 TEMPORARY_FILE="$(mktemp --suffix StatusUpdate)"
 
-touch "$TODAYS_MESSAGE_FILE"
+touch "$CHOSEN_MESSAGE_FILE"
 
-cat "$TODAYS_MESSAGE_FILE" > "$TEMPORARY_FILE"
+cat "$EDITOR_MESSAGE_FILE" >> "$TEMPORARY_FILE"
+cat "$CHOSEN_MESSAGE_FILE" >> "$TEMPORARY_FILE"
 
-if [[ -z "$1" ]]; then
+if [[ -z "$MESSAGE" ]]; then
 	if [[ -z "$EDITOR" ]]; then
 		vi "$TEMPORARY_FILE"
 	else
 		$EDITOR "$TEMPORARY_FILE"
 	fi
 else
-	echo "$1" >> "$TEMPORARY_FILE"
+	echo "$MESSAGE" >> "$TEMPORARY_FILE"
 fi
 
-cat "$TEMPORARY_FILE" > "$TODAYS_MESSAGE_FILE"
+cat "$TEMPORARY_FILE" | grep -v '^\s*#' > "$CHOSEN_MESSAGE_FILE"
 
 rm "$TEMPORARY_FILE"
