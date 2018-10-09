2bb2b5942405a5eadf9ba9033d5fc3e7dbc3c9dc
James Ward <james@notjam.es>
2018-10-09 13:39:01 -0400

    add a quick helper for macos setup

---
+++ home/.setup/macos.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+brew install python python2
+brew install thefuck
+brew install kubernetes-helm
+brew install yaml
+brew cask install meld
+brew cask install jetbrains-toolbox
+brew cask install google-chrome-canary
+brew cask install iterm2
+brew cask install karabiner-elements
