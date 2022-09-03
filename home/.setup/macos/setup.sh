9e97e8bf9a43cb8faa28062709783448f1f6aa24
James Ward <james@notjam.es>
2022-09-02 23:30:19 -0400

    migrate to a git-based system

--- home/.setup/macos/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew tap "homebrew/bundle"
-
-brew bundle install --file $(dirname "$0")/Brewfile
-
